e99e4786a798a28a4e4ca856f4e0b31513bde159
Yar Sher <yarser.dev@gmail.com>
2019-01-28 14:48:26 -0500

    Initial Commit

---
+++ MemeMe1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe1.0
+//
+//  Created by Yar Sher on 1/28/19.
+//  Copyright © 2019 Yar Sher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
